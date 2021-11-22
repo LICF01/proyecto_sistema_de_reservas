18e1c628cb07f422f8a29f5be5f396bd021ba5ae
Lucas Cubilla <lucascubilla@gmail.com>
2021-11-22 03:13:04 -0300

    Añadido acceso al servicio Cliente en la firma del ConfigRunner.  Se accede al servicio get del servicio cliente que devuelve el cliente con el ID dado

--- src/main/java/com/Hotel_booking/WebApp/reserva/ReservaConfig.java
+++ src/main/java/com/Hotel_booking/WebApp/reserva/ReservaConfig.java
@@ -1,12 +1,11 @@
 package com.Hotel_booking.WebApp.reserva;
 
+import com.Hotel_booking.WebApp.cliente.ClienteService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 
@@ -14,9 +13,10 @@ import java.util.Arrays;
 public class ReservaConfig {
 
     @Bean
-    CommandLineRunner reservaConfigRunner(ReservaRepository reservaRepository) {
+    CommandLineRunner reservaConfigRunner(ReservaRepository reservaRepository, ClienteService clienteService) {
         return args -> {
             Reserva reserva = new Reserva(
+                    clienteService.get(3L),
                     new SimpleDateFormat("dd/MM/yyyy").parse("15/03/2021"),
                     1,
                     2,
