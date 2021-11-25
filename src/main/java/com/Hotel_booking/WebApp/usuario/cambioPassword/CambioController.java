f70872cb369fe97b64646dc1b86506afed1e63d6
Katia-AR <92684615+Katia-AR@users.noreply.github.com>
2021-11-25 15:00:34 -0300

    Create CambioController.java

---
+++ src/main/java/com/Hotel_booking/WebApp/usuario/cambioPassword/CambioController.java
@@ -0,0 +1,65 @@
+package com.Hotel_booking.WebApp.usuario.cambioPassword;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequestMapping(path = "api/forgot-password")
+public class CambioController
+{
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private CambioService cambioService;
+
+    @PostMapping()
+    public String forgotPassword(@RequestParam String email, HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
+        String response = cambioService.forgotPassword(email);
+
+        if (!response.startsWith("Invalid"))
+        {
+            response = Utility.getSiteURL(request) + "/api/forgot-password/reset-password?token=" + response;
+
+            sendEmail(email, response);
+        }
+
+        return response;
+    }
+
+    @PutMapping("/reset-password")
+    public String resetPassword(@RequestParam String token, @RequestParam String password)
+    {
+        return cambioService.resetPassword(token,password);
+    }
+
+    public void sendEmail(String email, String enlace) throws MessagingException, UnsupportedEncodingException
+    {
+        MimeMessage mensaje = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mensaje);
+
+        helper.setFrom("posadaLapachos@gmail.com", "Soporte Lapachos");
+        helper.setTo(email);
+
+        String subject = "Enlace para restablecimiento de contraseña";
+
+        String contenido = "<p> Ha solicitado restablecer su contraseña </p>"
+                + "<p> Haga click en el enlace de abajo para poder cambiar su contraseña: </p>"
+                + "<p><a href=\"" + enlace + "\"> Cambiar mi contraseña </a></p>" + "<br>" +
+                "<p>Ignora este correo si usted no ha solicitado el cambio de contraseña</p>";
+
+        helper.setSubject(subject);
+
+        helper.setText(contenido, true);
+
+        mailSender.send(mensaje);
+    }
+
+}
