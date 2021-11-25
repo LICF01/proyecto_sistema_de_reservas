29e7a7cb8a8cc55a317a6a1e408be506dbbda441
Lucas Cubilla <lucascubilla@gmail.com>
2021-11-25 01:02:02 -0300

    frontend base

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Layout from "./components/Layout";
-import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      {/* <Layout /> */}
-      <Login />
+      <Layout />
     </div>
   );
 }
