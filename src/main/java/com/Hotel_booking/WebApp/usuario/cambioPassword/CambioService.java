11218922ddf4970a5e50b148134ecf76ca8ea298
Katia-AR <92684615+Katia-AR@users.noreply.github.com>
2021-11-26 13:09:20 -0300

    Cambios minimos
    Se ha agregado comentarios explicando más o menos las cosas que aún no se han visto.

--- src/main/java/com/Hotel_booking/WebApp/usuario/cambioPassword/CambioService.java
+++ src/main/java/com/Hotel_booking/WebApp/usuario/cambioPassword/CambioService.java
@@ -2,7 +2,6 @@ package com.Hotel_booking.WebApp.usuario.cambioPassword;
 
 import com.Hotel_booking.WebApp.exceptions.CustomErrorException;
 import com.Hotel_booking.WebApp.usuario.Usuario;
-import com.Hotel_booking.WebApp.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,27 +17,35 @@ public class CambioService
 {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private CambioRepository cambioRepository;
 
     public String forgotPassword(String email)
     {
+        //Busca si el email esta asociado a un usuario
         Optional<Usuario> userOptional = Optional
-                .ofNullable(usuarioRepository.findByEmail(email));
+                .ofNullable(cambioRepository.findByEmail(email));
 
+        //En caso de que no se encuentre presente retornara "Email invalido"
         if(!userOptional.isPresent())
         {
             return "Email invalido";
         }
 
         Usuario usuario = userOptional.get();
+
+        // A "usuario" se le cargara el valor del token de acuerdo a lo que devuelva el método "generateToken"
         usuario.setTokenPassword(generateToken());
+
+        //A la fecha de cambio de contraseña se le asigna la fecha en el que suceda dicha acción
         usuario.setFechaCambioContrasena(LocalDate.now());
 
-        usuario = usuarioRepository.save(usuario);
+        usuario = cambioRepository.save(usuario);
 
+        //Retorna el token para utilizar en el cambio de contraseña
         return usuario.getTokenPassword();
     }
 
+    //Se encarga de generar el Token de forma pseudo criptográfica
     private String generateToken()
     {
         String token = UUID.randomUUID().toString();
@@ -48,7 +55,7 @@ public class CambioService
 
     public String resetPassword(String tokenPassword, String password) {
         Optional<Usuario> userOptional = Optional
-                .ofNullable(usuarioRepository.findUsuarioByToken(tokenPassword));
+                .ofNullable(cambioRepository.findUsuarioByToken(tokenPassword));
 
         if (!userOptional.isPresent())
         {
@@ -65,15 +72,16 @@ public class CambioService
             throw new CustomErrorException(HttpStatus.BAD_REQUEST, "La contraseña debe ser de al menos 6 caracteres");
         }
 
+        //Se encripta la nueva contraseña
+
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(password);
 
         user.setContrasena(encodedPassword);
 
         user.setTokenPassword(null);
-        user.setFechaCambioContrasena(null);
 
-        usuarioRepository.save(user);
+        cambioRepository.save(user);
 
         return "Se ha actualizado su contraseña";
     }
