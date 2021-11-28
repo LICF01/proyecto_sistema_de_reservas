bf05c5ae3f1edcfea3547a1d3c2844caed816d3b
Lucas Cubilla <lucascubilla@gmail.com>
2021-11-28 16:36:31 -0300

    Añadida dependencia javax e imports necesarios

--- src/main/java/com/Hotel_booking/WebApp/Habitacion/Habitacion.java
+++ src/main/java/com/Hotel_booking/WebApp/Habitacion/Habitacion.java
@@ -1,11 +1,9 @@
 package com.Hotel_booking.WebApp.Habitacion;
 
-
-//import jakarta.validation.constraints.Min;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
-//import jakarta.validation.constraints.Positive;
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
